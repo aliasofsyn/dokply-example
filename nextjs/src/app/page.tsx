fb69a529b32bebfe9cc525150756bf17dab8d9b5
aliasofsyn <ssefali301@gmail.com>
2025-10-03 12:48:20 -0400

    First changes in on my deployed example. Hopefully it updates on our hosted server

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -3,6 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
 	<div>
-		This is youssefs homepage
-	<div/>
+	<h1>
+	This is Youssefs home page.
+	</h1>
+<p>We are learning how to work with dockploy in order to make contribution to certain issues in the future.</p>
+<p>We are also learning how to work with neovim and getting all our plugins sorted!<p/>
+<div/>
 )
